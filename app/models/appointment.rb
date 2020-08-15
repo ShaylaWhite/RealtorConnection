e5bf6eab486e407f0603831efbd4cbaa68a3e4ff
ShaylaWhite <shayladwhite1@gmail.com>
2020-08-15 15:47:55 -0700

    validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,6 @@ class Appointment < ApplicationRecord
     belongs_to :houses
     belongs_to :users 
 
-    validates :email, presence: true, uniqueness: true 
+    validates :time, presence: true
      
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     has_many :appointments
     has_many :houses, through :appointments
+
+    validates :email, presence: true, uniqueness: true 
+        
+
 end
