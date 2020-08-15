ad6bc82d1028246adb1518aacf3b6525b75ae0ec
ShaylaWhite <shayladwhite1@gmail.com>
2020-08-15 15:42:36 -0700

    validations/bycrptgem

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
     belongs_to :houses
     belongs_to :users 
+
+    validates :email, presence: true, uniqueness: true 
+     
 end
--- app/models/house.rb
+++ app/models/house.rb
@@ -2,4 +2,6 @@ class House < ApplicationRecord
     belongs_to :user
     has_many :appointments
     has_many :users, through appointments 
+
+    validates :name, presence: true
 end
