30fcd4e0bd0a8089c0c2252802d0d29087a5217f
ShaylaWhite <shayladwhite1@gmail.com>
2020-08-17 10:26:25 -0700

    links on home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
-    before_action :auth
-
-    def auth
-        @current_user = User.find_by(:id session[:user_id])
-    end
-
-
-    def home
-        redirect_to login_path
-    end 
+   
 end
